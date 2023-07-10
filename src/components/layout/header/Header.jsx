9fceccda083d333ce9b2d4201b9c74d970e236be
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2023-07-11 00:54:01 +0600

    Added header

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,8 +1,122 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
 import styles from "./Header.module.scss";
 
 const Header = () => {
-  return <></>;
+  return (
+    <>
+      <div className={styles.navbar}>
+        <Link to="/" className={`${styles.link} ${styles.button}`}>
+          <div className={styles.logo}>ChinatradeX</div>
+        </Link>
+
+        <div className={styles.middle}>
+          <div className={styles.links}>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? `${styles.link} ${styles.active_link}` : styles.link
+              }>
+              О компании
+            </NavLink>
+
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? `${styles.link} ${styles.active_link}` : styles.link
+              }>
+              Товары
+            </NavLink>
+
+            <NavLink
+              to="/deliver"
+              className={({ isActive }) =>
+                isActive ? `${styles.link} ${styles.active_link}` : styles.link
+              }>
+              Доставка
+            </NavLink>
+
+            <div className={styles.order}>
+              <p className={styles.text}>Заказать </p>
+
+              <div className={styles.dropdown}>
+                <div className={styles.menu}>
+                  <div className={styles.list}>
+                    <NavLink
+                      to="/alibaba"
+                      className={({ isActive }) =>
+                        isActive
+                          ? `${styles.menu_link} ${styles.active}`
+                          : styles.menu_link
+                      }>
+                      <h1 className={styles.title}>
+                        Alibaba <span>НОВЫЙ</span>
+                      </h1>
+                      <p className={styles.desc}>
+                        Транснациональный конгломерат электронной коммерции
+                      </p>
+                    </NavLink>
+
+                    <NavLink
+                      to="/1688"
+                      className={({ isActive }) =>
+                        isActive
+                          ? `${styles.menu_link} ${styles.active}`
+                          : styles.menu_link
+                      }>
+                      <h1 className={styles.title}>1688</h1>
+                      <p className={styles.desc}>
+                        Оптовая торговая площадка Alibaba
+                      </p>
+                    </NavLink>
+
+                    <NavLink
+                      to="/taobao"
+                      className={({ isActive }) =>
+                        isActive
+                          ? `${styles.menu_link} ${styles.active}`
+                          : styles.menu_link
+                      }>
+                      <h1 className={styles.title}>Taobao</h1>
+                      <p className={styles.desc}>
+                        Популярная китайская платформа электронной торговли
+                        между потребителями платформа
+                      </p>
+                    </NavLink>
+
+                    <NavLink
+                      to="/poizon"
+                      className={({ isActive }) =>
+                        isActive
+                          ? `${styles.menu_link} ${styles.active}`
+                          : styles.menu_link
+                      }>
+                      <h1 className={styles.title}>Poizon</h1>
+                      <p className={styles.desc}>Платформы Alibaba и Poizon</p>
+                    </NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.right}>
+          <div className={styles.buttons}>
+            <Link to="/login" className={styles.button}>
+              Войти
+            </Link>
+
+            <Link
+              to="/register"
+              className={`${styles.button} ${styles.active}`}>
+              Регистрация
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -4,7 +4,30 @@ import styles from "./Home.module.scss";
 const Home = () => {
   return (
     <>
-      <div>Home</div>
+      <div className={styles.page_wrapper}>
+        {/* <div className={styles.buttons}>
+          <div className={styles.button}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+              viewBox="0 0 24 24"
+              style={{ fill: "#fff" }}>
+              <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path>
+            </svg>
+          </div>
+          <div className={styles.button}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              style={{ fill: "#fff" }}>
+              <path d="M10.061 19.061 17.121 12l-7.06-7.061-2.122 2.122L12.879 12l-4.94 4.939z"></path>
+            </svg>
+          </div>
+        </div> */}
+      </div>
     </>
   );
 };
